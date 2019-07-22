4d089080e4839d4ea0a0a2ab5d51ce178e5c068c
Josie Nagy <josie@bitcrowd.net>
2019-07-22 16:33:27 +0200

    Add event listener that's triggered when user clicks 'roll dice' button
    Add an anonymous function inside addEventListener() method that creates the dice value, and then displays the correct png image depending on the value you get.

--- JS_Basic_Challenges/4-DOM-pig-game/starter/app.js
+++ JS_Basic_Challenges/4-DOM-pig-game/starter/app.js
@@ -11,25 +11,54 @@ GAME RULES:
 
 */
 
-var totalScore, currentScore, activePlayer, dice;
+var totalScore, currentScore, activePlayer;
 
 var totalScore = [0,0];     // the total score each player has, added together, after multiple rounds of rolling the dice
 var currentScore = 0;       // the score each player has per round/ session, when it's they're actively rolling the dice 
 var activePlayer = 0;       // the player that is currently rolling the dice (can either be 0 or 1)
 
-dice = Math.floor(Math.random() * 6) + 1;  // create a dice that generates a random number from 1 to 6 (included)
-
 // Setting a value
 // Use the 'querySelector' method to select and change the value of the html element with the id starting with "current-". 
-// The 'textContent' property sets or returns the text content of the node you specify. In our case, that's the dice value of the player rolling it.
+// The 'textContent' property sets the text content of the node you specify. In our case, that's the dice value of the player rolling it.
 // Use concatenation (+) to create a dynamic class; the player can either be 0 (id="current-0") or 1 (id="current-1").
-document.querySelector('#current-'  + activePlayer).textContent = dice;
+// document.querySelector('#current-'  + activePlayer).textContent = dice;
 // Longer version: document.querySelector('#current-' + activePlayer).textContent = Math.floor(Math.random() * 6) + 1;
 
 // Getting a value
 // Read the value or the content of the element with this id (id="score-0"), and then store it into variable x.
-var x = document.querySelector('#score-0').textContent;
+// var x = document.querySelector('#score-0').textContent;
 
 // Change the style of the dice using the style method and selecting the '.dice' class
 // The dice is now hidden when user first loads the page and hasn't started rolling the dice yet.
 document.querySelector('.dice').style.display = 'none';
+
+
+// Add the event listener to the button that rolls the dice, using the .addEventListener method
+// An event listener is a function that is only triggered (by the user) when a specific event happens (click, scroll, ...). 
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+  // 1. Create a random number from 1 to 6 (included) that imitates throwing a dice.
+
+  // The .random method generates a number from 0 to 1 (ex: 0.5678)
+  // The .floor method rounds a number downwards to it's nearest integer (ex: from 2.3 to 2)
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result on the page.
+
+  // Select the .dice class from the DOM and store it in a variable.
+  var diceDOM = document.querySelector('.dice');
+
+  // Make it visible again (the dice is initially hidden).
+  diceDOM.style.display = 'block';
+
+  // Display the correct number (from 1 to 6), depending on which dice value the user gets.
+  // Set a different png for every dice value from 1 to 6.
+  // The src property sets the value of the src attribute of our png.
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // 3. Update the new value after rolling the dice, as long as the rolled number is NOT 1 (see game rules above).
+
+});
+
+// document.querySelector('#current-'  + activePlayer).textContent = dice;
+// var x = document.querySelector('#score-0').textContent;
