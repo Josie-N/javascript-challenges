55a315b209bf3e15dbe59cabce5c015c051e8f00
Josie <nagy.iozefina@yahoo.com>
2019-09-01 14:48:01 +0200

    Add challenge requirements

---
+++ JS_Basic_Challenges/level-beginner/5th-Challenge.js
@@ -0,0 +1,54 @@
+/*
+
+Level: Beginner
+
+CODING CHALLENGE 5
+
+
+
+
+You can use the map method to transform each item in an array into something else.
+map() returns a new array leaving the original array unchanged.
+
+Challenge 1:
+
+We have an array of numbers that are stored as strings.
+// Initial: [ '1', '2', '3', '4', '5' ];
+
+Let's transform these strings into numbers using the map method.
+// Result: [ 1, 2, 3, 4, 5 ];
+
+Then store the new array we created in a variable.
+
+--------------------------
+
+Challenge 2:
+
+We have an array of words that are stored in as strings.
+// Initial: [ 'apple', 'pear', 'cherry' ];
+
+Let's capitalize all the words we have within this array.*
+// Result: [ 'APPLE', 'PEAR', 'CHERRY' ];
+
+Then store the new array we created in a variable.
+
+* The words will still be strings, just as before.
+
+--------------------------
+
+Challenge 3:
+
+Turn this list of numbers into price strings with two digits and a dollar sign at the beginning.
+// Initial: [5, 4.23, 6.4, 8.09, 3.20];
+
+Dont forget to:
+- Turn the numbers into strings
+- Concatenate/ add the dollar sign 
+- Make the prices have decimals
+- Store the new array we created in a variable
+// Result: [ '$5.00', '$4.23', '$6.40', '$8.09', '$3.20' ];
+
+
+SOLUTION 👇🏼 
+
+*/
