787de3e9474c86ddcc70ddbd1023b8fd19485b3f
Josie <nagy.iozefina@yahoo.com>
2019-09-29 21:38:32 +0200

    Add more comments

--- individual_challenges/level-beginner/student-records-challenge/js/students.js
+++ individual_challenges/level-beginner/student-records-challenge/js/students.js
@@ -1,10 +1,16 @@
-// Access each student's record in the array, and print out the students name, 
+/*
+
+Level: Intermediate
+
+Student Records Challenge
+
+
+
+// Access each student's record from the array, and print out the students name, 
 // track, points, and achievements.
 
 // The finished project should display a report containing a section with each 
 // student's info: name at the top, and their track, points, and achievements.
-
-// To do this, you'll need to use a loop.
 
 // 1. Loop through each element of the array
 // 2. Build up a message that contains all of the student records
@@ -18,6 +24,56 @@
 
 // But in this case, you'll be accessing array elements.
 
+
+
+SOLUTION 👇🏼 
+
+*/
+
+
+
+
+
+
+
+
+
+
+// With comments: 
+
+
+var message = "";
+var student;
+
+// Create a function that will update the message into the DOM
+function print(message) {
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = message;
+}
+
+// Our array: var students = [{},{},{},{},{}];
+
+// Access each student object using a 'for' loop
+for (var i = 0; i < students.length; i += 1) {   // result: 0, 1, 2, 3, 4  (students.length = 5)
+  // Store each student object in a variable
+  var student = students[i];   // result: students[0], students[1], students[2], students[3], students[4]
+  // Build up a message that contains all of the student records
+  message += '<h2>Student: ' + student.name + '</h2>';
+  message += '<p>Track: ' + student.track + '</p>';
+  message += '<p>Achievements: ' + student.achievements + '</p>';
+  message += '<p>Points: ' + student.points + '</p>';
+}
+// Print the message to the page
+print(message);
+
+
+
+
+/* ------------------------------------ */
+
+// Without comments: 
+
+
 var message = "";
 var student;
 
@@ -26,12 +82,8 @@ function print(message) {
   outputDiv.innerHTML = message;
 }
 
-// Our array: var students = [{},{},{},{},{}];
-
-// access each student object using a 'for' loop
-for (var i = 0; i < students.length; i += 1) {  // result: 0, 1, 2, 3, 4  (students.length = 5)
-  // store each student object in a variable
-  var student = students[i];  // result: students[0], students[1], students[2], students[3], students[4]
+for (var i = 0; i < students.length; i += 1) { 
+  var student = students[i];
   message += '<h2>Student: ' + student.name + '</h2>';
   message += '<p>Track: ' + student.track + '</p>';
   message += '<p>Achievements: ' + student.achievements + '</p>';
