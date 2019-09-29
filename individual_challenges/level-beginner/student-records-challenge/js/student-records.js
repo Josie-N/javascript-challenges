fc7ef4075b3b124ac93a7d4479e52f9eecf91336
Josie <nagy.iozefina@yahoo.com>
2019-09-29 15:08:39 +0200

    Add file structure, challenge instructions, JSON with student data
    And some styling for the text that will be displayed when the challenge is completed.

---
+++ individual_challenges/level-beginner/student-records-challenge/js/student-records.js
@@ -0,0 +1,32 @@
+var students = [
+  {
+    name : "Josie",
+    track : "Python Development",
+    achievements : 108,
+    points : 11500
+  },
+  {
+    name : "Caleb",
+    track : "iOS Development with Swift",
+    achievements : 68,
+    points : 18099
+  },
+  {
+    name : "Andal",
+    track : "Design Thinking",
+    achievements : 73,
+    points : 9960
+  },
+  {
+    name : "Tara",
+    track : "Front End Development",
+    achievements : 530,
+    points : 40021
+  },
+  {
+    name : "Jonathan",
+    track : "JavaScript Basics",
+    achievements : 44,
+    points : 2504
+  },
+];
---
+++ individual_challenges/level-beginner/student-records-challenge/js/students.js
@@ -0,0 +1,17 @@
+// Access each student's record in the array, and print out the students name, 
+// track, points, and achievements.
+
+// The finished project should display a report containing a section with each 
+// student's info: name at the top, and their track, points, and achievements.
+
+// To do this, you'll need to use a loop.
+
+// Loop through each element of the array, and build up a message that contains 
+// all of the student records, then print that message to the page.
+
+// The tricky part, is getting to the actual property value of each object.
+// Remember, we can use dot notation to access a property of an object.
+// For example, student.name, accesses the name property of an object name 
+// student.
+
+// But in this case, you'll be accessing array elements.
